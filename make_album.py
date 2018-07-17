76d462d3897d6c26a68dddb451a2f72410286d07
Tangue Muweh <atmuweh@gmail.com>
2018-07-17 13:50:04 +0200

    Adds optinal parameter 'track' to make_album()

--- make_album.py
+++ make_album.py
@@ -1,9 +1,11 @@
 
-def make_album(artist_name, album_title):
+def make_album(artist_name, album_title, tracks=""):
 	
 	album = {"artist_name": artist_name.title(), "album_title": album_title.title()}
+	if tracks:
+		album["tracks"] = tracks
+
 	return album
 
-print(make_album("flavour", "divine"))
-print(make_album("semah", "divine"))
-print(make_album("don moen", "Grace"))
+print(make_album(artist_name ="flavour", album_title = "divine", tracks = 5))
+print(make_album(artist_name = "don moen", album_title = "Grace"))
