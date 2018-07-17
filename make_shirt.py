02931e694db04d212224a14b1e17cab20ce3190e
Tangue Muweh <atmuweh@gmail.com>
2018-07-17 11:28:15 +0200

    passing function arguements with position and keyword

---
+++ make_shirt.py
@@ -0,0 +1,8 @@
+"""Make a shirt for"""
+def make_shirt(t_size, message):
+	summary = "Size: " + t_size + "; message: " + message + "!"
+
+	print(summary)
+
+make_shirt("XL", "I Love Python")
+make_shirt(message ="I love Python 3", t_size = "M")
