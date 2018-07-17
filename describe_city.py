115ac9a32b65049b59b76963d19c2bd9915702af
Tangue Muweh <atmuweh@gmail.com>
2018-07-17 11:58:03 +0200

    Default value

---
+++ describe_city.py
@@ -0,0 +1,9 @@
+
+def describe_city(city, country = "Ambazonia"):
+	sentence = city.title() + " is in " + country.title() + "."
+
+	print(sentence)
+
+describe_city(city="limbe")
+describe_city(city="Bamenda")
+describe_city(city="Adisabba")
