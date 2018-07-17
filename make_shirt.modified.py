04f309270afef02dc6754ee4bbf351497a4da5c7
Tangue Muweh <atmuweh@gmail.com>
2018-07-17 11:38:26 +0200

    impliments default value in function parametes in make_shirts

---
+++ make_shirt.modified.py
@@ -0,0 +1,10 @@
+"""Make a shirt for"""
+def make_shirt(t_size = "L", message = "I love Python"):
+	summary = "Size: " + t_size + "; message: " + message + "!"
+
+	print(summary)
+
+make_shirt(t_size ="L")
+make_shirt(t_size ="M")
+make_shirt(message ="I am coding in Python")
+
