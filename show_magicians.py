60bbb8a221151453670d3fd57dd32eb6adc386e8
Tangue Muweh <atmuweh@gmail.com>
2018-07-17 16:24:26 +0200

    passed copy of list to function

--- show_magicians.py
+++ show_magicians.py
@@ -1,17 +1,18 @@
 
-def show_magicians(modified_magicians):
+def show_magicians(magicians, modified_magicians):
 
-	for name in modified_magicians:
-
-		print(name)
+	print(magicians)
+	print(modified_magicians)
 
 def make_great(magicians, modified_magicians):
 
 	for name in magicians:
 		modified_magicians.append("the Great " + name.title())
 
+	return modified_magicians
+
 magicians = ['tangue', 'aburo', 'agbor', 'dona']
 modified_magicians = []
 make_great(magicians[:], modified_magicians)
-show_magicians(modified_magicians)
+show_magicians(magicians, modified_magicians)
 
