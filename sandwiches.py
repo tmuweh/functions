fa91cb29d80b6711a294507aa053b54c9f3c50eb
Tangue Muweh <atmuweh@gmail.com>
2018-07-17 17:23:38 +0200

    varible paramenter in function

---
+++ sandwiches.py
@@ -0,0 +1,8 @@
+
+def make_sandwiches(*toppings):
+
+	for item in toppings:
+		print("- "+ item)
+make_sandwiches('mayonaise', 'korn', 'mjolk')
+make_sandwiches('oil', 'salt')
+make_sandwiches('guka', 'tomat', 'lok', 'peppar', 'gron')
