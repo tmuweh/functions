d3da5a7f89634d8932a4df23c6e20cbb0b4a5afc
Tangue Muweh <atmuweh@gmail.com>
2018-07-19 01:08:51 +0200

    printing module for addition and multiplicaiton of two numbers

---
+++ print_models.py
@@ -0,0 +1,4 @@
+import printing_functions
+
+printing_functions.print_sum(19, 10)
+printing_functions.print_product(2, 2)
---
+++ printing_functions.py
@@ -0,0 +1,8 @@
+
+def print_sum(num1, num2):
+	"""prints the sum of two numbers"""
+	print(num1 + num2)
+
+def print_product(num1, num2):
+	"""Print the product of two numbers"""
+	print(num1 * num2)
