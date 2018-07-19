b21d44190a633b5eee6a8abd14e20698a00c21c7
Tangue Muweh <atmuweh@gmail.com>
2018-07-19 13:31:23 +0200

    creates 3 instance of Restaurant class

--- restaurant.py
+++ restaurant.py
@@ -10,10 +10,25 @@ class Restaurant(object):
 	def describe_restaurant(self):
 		"""prints information about restaurant"""
 		print("Name: " + self.restaurant_name)
-		
+		print("Cuisine Type: " + self.cuisine_type)
 
 	def open_restaurant(self):
 		"""display information whether restaurant is open"""
-		print("Cuisine Type: " + self.cuisine_type)
+		print(self.restaurant_name + " is open now")
 		
+
+umea = Restaurant("Saint David", "African")
+umea.describe_restaurant()
+umea.open_restaurant()
+
+
+stockholm = Restaurant("Erriksson", "Western")
+stockholm.describe_restaurant()
+stockholm.open_restaurant()
+
+uppsala = Restaurant("ThaiRestaurant", "ASian")
+uppsala.describe_restaurant()
+uppsala.open_restaurant()
+
+
 
