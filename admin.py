c1486ccf2cb6d059f540f4494bf85b3aef3a21cf
Tangue Muweh <atmuweh@gmail.com>
2018-07-20 01:51:15 +0200

    moves privilege attributes and method to Privilege class

--- admin.py
+++ admin.py
@@ -1,20 +1,27 @@
 from user import User
 
-class Admin(User):
-	"""docstring for Admin"""
-	def __init__(self, first_name, last_name, *privileges):
-		super(Admin, self).__init__(first_name, last_name)
+class Privileges(object):
+	"""docstring for privilege"""
+	def __init__(self, *privileges):
 		self.privileges = []
 		for privilege in privileges:
 			self.privileges.append(privilege)
 
-	def show_privileges(self):
 
+	def show_privileges(self):
 		for privilege in self.privileges:
 			print("\n- " + privilege)
 
+class Admin(User):
+	"""docstring for Admin"""
+	def __init__(self, first_name, last_name):
+		super(Admin, self).__init__(first_name, last_name)
+		self.privileges = Privileges("can add post", "can delete post", "can add user", "can remove user/privileges")
 
-user1 = Admin("anselm", "Muweh", "can add post", "can delete post", "can add user", "can remove user/privileges")
+	
+
+
+user1 = Admin("anselm", "Muweh" )
 
 user1.describe_user()
-user1.show_privileges()
+user1.privileges.show_privileges()
