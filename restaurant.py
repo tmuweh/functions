662dc86f784b9920616d9ae0872bf6949222a53e
Tangue Muweh <atmuweh@gmail.com>
2018-07-19 13:07:02 +0200

    creats the Restaurant class with two methods

---
+++ restaurant.py
@@ -0,0 +1,18 @@
+
+class Restaurant(object):
+	"""docstring for Restaurant"""
+	def __init__(self, restaurant_name, cuisine_type):
+
+		self.restaurant_name = restaurant_name
+		self.cuisine_type = cuisine_type
+		
+
+	def describe_restaurant(self):
+		"""prints information about restaurant"""
+		pass
+
+	def open_restaurant(self):
+		"""display information whether restaurant is open"""
+		pass
+
+	
