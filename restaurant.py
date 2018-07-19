ae0b00f78a7fb11b2c56c206b0a5ea0c1cdf514d
Tangue Muweh <atmuweh@gmail.com>
2018-07-19 15:47:24 +0200

    change attribute of objects using methods as well as directly using attributes

--- restaurant.py
+++ restaurant.py
@@ -5,6 +5,7 @@ class Restaurant(object):
 
 		self.restaurant_name = restaurant_name
 		self.cuisine_type = cuisine_type
+		self.number_served = 0
 		
 
 	def describe_restaurant(self):
@@ -15,20 +16,18 @@ class Restaurant(object):
 	def open_restaurant(self):
 		"""display information whether restaurant is open"""
 		print(self.restaurant_name + " is open now")
-		
+	def set_number_served(self, number_served):
+		"""sets a new number serve for restaurant class"""
+		self.number_served = number_served
+	def increment_number_sesrved(self, increment):
+		"""increments number_served by increment"""
+		self.number_served += increment
 
-umea = Restaurant("Saint David", "African")
-umea.describe_restaurant()
-umea.open_restaurant()
-
-
-stockholm = Restaurant("Erriksson", "Western")
-stockholm.describe_restaurant()
-stockholm.open_restaurant()
-
-uppsala = Restaurant("ThaiRestaurant", "ASian")
-uppsala.describe_restaurant()
-uppsala.open_restaurant()
+restaurant = Restaurant("restau", "general")
+restaurant.set_number_served(40)
+print(restaurant.number_served)
+restaurant.increment_number_sesrved(10)
+print(restaurant.number_served)
 
 
 
