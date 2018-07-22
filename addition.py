6c8be01c81b8781d91c54e0a054f5810291a8793
Tangue Muweh <atmuweh@gmail.com>
2018-07-23 00:37:36 +0200

    pass exception to permit continuation of program

--- addition.py
+++ addition.py
@@ -3,12 +3,19 @@ def addition(num1, num2):
 	"""add two numbers """
 	try:
 		sum = int(num1) + int(num2)
-	except TypeError:
-		print("You cannot sum strings")
+	except Exception as e:
+		#do nothing but move on
+		pass
 	else:
 		print("Sum is " + str(sum))
 
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
 
-addition(num1, num2)
+while True:
+	print("Enter q at anytime to quit to quit!")
+	num1 = input("Enter a number: ")
+	if num1 == 'q':
+		break
+	num2 = input("Enter another number: ")
+	if num2 == 'q':
+		break
+	addition(num1, num2)
