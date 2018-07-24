ec3b7a011343014e037417dc7b088db0a921cb06
Tangue Muweh <atmuweh@gmail.com>
2018-07-24 22:11:44 +0200

    setup a setUp(), preventing creation of two objects of employee class

--- test_employee.py
+++ test_employee.py
@@ -4,14 +4,19 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 	"""docstring for TestEmployee"""
+
+	def setUp(self):
+
+		self.empl = Employee("Tangue", "Muweh", 100000)
+
 	def test_give_default_raise(self):
-		empl = Employee("Tangue", "Muweh", 100000)
-		empl.give_raise() #default value in function  is 5000
-		self.assertEqual(empl.annual_salary, 105000)
+		
+		self.empl.give_raise() #default value in function  is 5000
+		self.assertEqual(self.empl.annual_salary, 105000)
 	def test_give_custom_raise(self):
-		empl = Employee("Tangue", "Muweh", 200000)
-		empl.give_raise(10000)
-		self.assertEqual(empl.annual_salary, 210000)
+
+		self.empl.give_raise(10000)
+		self.assertEqual(self.empl.annual_salary, 110000)
 
 unittest.main()
 
